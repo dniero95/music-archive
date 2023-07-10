c82dca3cb2c8f0498a04779fff3edd756a059d9c
Dario Niero <dario.niero@enginius.com>
2023-07-11 01:37:52 +0200

    update tag repo

--- musicarchive/src/main/java/its/darioniero/musicarchive/repositories/TagRepository.java
+++ musicarchive/src/main/java/its/darioniero/musicarchive/repositories/TagRepository.java
@@ -1,9 +1,14 @@
 package its.darioniero.musicarchive.repositories;
 
-import its.darioniero.musicarchive.entities.Song;
+
 import its.darioniero.musicarchive.entities.Tag;
 import its.darioniero.musicarchive.entities.TagId;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TagRepository extends CrudRepository<Tag, TagId> {
+import java.util.List;
+
+public interface TagRepository extends CrudRepository<Tag, Long> {
+    @Query("SELECT t FROM Tag t ORDER BY t.weight DESC, t.tag ASC")
+    List<Tag> findAllByWeightLimitedTo100();
 }
