aac7501aae486ab1442cc61f166b5251c90aec0c
Dario Niero <dario.niero@enginius.com>
2023-07-10 23:18:25 +0200

    init spring project

---
+++ musicarchive/src/main/java/its/darioniero/musicarchive/MusicarchiveApplication.java
@@ -0,0 +1,13 @@
+package its.darioniero.musicarchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicarchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicarchiveApplication.class, args);
+	}
+
+}
---
+++ musicarchive/src/test/java/its/darioniero/musicarchive/MusicarchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package its.darioniero.musicarchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicarchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
