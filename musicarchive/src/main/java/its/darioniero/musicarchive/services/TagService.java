740b803cc12a745f429fa96e4ddc685ebc783826
Dario Niero <dario.niero@enginius.com>
2023-07-11 01:38:43 +0200

    create method fetchMostPopularTags

---
+++ musicarchive/src/main/java/its/darioniero/musicarchive/services/TagService.java
@@ -0,0 +1,25 @@
+package its.darioniero.musicarchive.services;
+
+import its.darioniero.musicarchive.entities.Tag;
+import its.darioniero.musicarchive.repositories.TagRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
+public class TagService {
+
+    @Autowired
+    TagRepository tagRepository;
+
+    public List<Tag> fetchMostPopularTags(){
+        return tagRepository.findAllByWeightLimitedTo100();
+    }
+
+}
