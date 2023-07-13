68d738479db00686dc535d79bbf94a6202f22883
Dario Niero <dario.niero@enginius.com>
2023-07-13 17:47:56 +0200

    add controller for index that redirect to tags view

--- musicarchive/src/main/java/its/darioniero/musicarchive/controllers/TagController.java
+++ musicarchive/src/main/java/its/darioniero/musicarchive/controllers/TagController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 
@@ -14,6 +15,11 @@ public class TagController {
 
     @Autowired
     TagService tagService;
+
+    @GetMapping
+    public RedirectView getIndexView(){
+        return new RedirectView("/tags");
+    }
 
     @GetMapping("/tags")
     public String getTagView(Model model){
